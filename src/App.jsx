e8703bc0019ed3adce26c90cf919c9aa0c0da22f
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-06-29 18:19:42 +0400

    router pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Main />}>
-        
+        <Route path="about-us" element={<About />} />
+        <Route path="contact-us" element={<Contact />} />
 
       </Route>
     </Routes>
